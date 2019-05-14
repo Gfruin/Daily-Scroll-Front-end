7e4e49e3833849d100fd1cbbf672443763600801
Greg Fruin <gfruin@gmail.com>
2019-05-14 12:16:03 -0500

    created ArticleContainer class in ArticleContainer/index.js

--- daily-scroll-frontend/src/ArticleContainer/index.js
+++ daily-scroll-frontend/src/ArticleContainer/index.js
@@ -1,1 +1,19 @@
-index.js
+import React, { Component } from 'react'
+
+
+class ArticleContainer extends Component {
+	constructor() {
+		super();
+		this.state = {}
+	}
+	render() {
+		return(
+			<div>
+				ArticleContainer
+			</div>
+
+		)
+	}
+}
+
+export default ArticleContainer;
