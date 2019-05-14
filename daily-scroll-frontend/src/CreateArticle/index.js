0daffc47334fd61693fc4f316ebeaa126e7c5c5f
Greg Fruin <gfruin@gmail.com>
2019-05-14 13:04:27 -0500

    added form submission for article title and description

--- daily-scroll-frontend/src/CreateArticle/index.js
+++ daily-scroll-frontend/src/CreateArticle/index.js
@@ -1,1 +1,33 @@
-index.js
+import React, { Component } from 'react';
+
+class CreateArticle extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			title: '',
+			description: ''
+
+		}
+	}
+	updateArticle = (e) => {
+		this.setState({[e.currentTarget.name]: e.currentTarget.value})
+	}
+
+	render(){
+		return (
+			<form>
+				<label>
+					Article:
+					<input type="text" name="title" onChange={this.updateArticle}/>
+				</label>
+				<label>
+					Description:
+					<input type="text" name="description" onChange={this.updateArticle}/>
+				</label>
+				<input type ="Submit"/>
+			</form>
+
+			)
+	}
+}
