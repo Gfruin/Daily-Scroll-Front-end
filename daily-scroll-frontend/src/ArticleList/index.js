aac8fedcfe63b005201b53343d3530238053161c
Greg Fruin <gfruin@gmail.com>
2019-05-14 18:00:24 -0500

    made Articles class in ArticleList

--- daily-scroll-frontend/src/ArticleList/index.js
+++ daily-scroll-frontend/src/ArticleList/index.js
@@ -1,1 +1,19 @@
-index.js
+import React from 'react'
+
+const Articles = (props) => {
+	const articleList = props.articles.map((article) => {
+		return (
+			<li key={movie._id}>
+			<span>{article.title}</span><br/>
+			<span>{article.description}</span><br/>
+			</li>
+
+			)
+	})
+	return(
+		<ul>
+			{articleList}
+		</ul>
+		)
+}
+export default Articles
