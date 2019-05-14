73c56468dbe4632154780215e3058704280a62a4
Greg Fruin <gfruin@gmail.com>
2019-05-14 13:19:23 -0500

    added select bar with news category options

--- daily-scroll-frontend/src/CreateArticle/index.js
+++ daily-scroll-frontend/src/CreateArticle/index.js
@@ -6,7 +6,8 @@ class CreateArticle extends Component {
 
 		this.state = {
 			title: '',
-			description: ''
+			description: '',
+			category: ''
 
 		}
 	}
@@ -25,7 +26,21 @@ class CreateArticle extends Component {
 					Description:
 					<input type="text" name="description" onChange={this.updateArticle}/>
 				</label>
-				<input type ="Submit"/>
+				<select name="category">
+  					<option value="education">Education</option> 
+  					<option value="technology" selected>Technology</option>
+  					<option value="politics">Politics</option>
+  					<option value="business">Business</option> 
+  					<option value="local" selected>Local</option>
+  					<option value="national">National</option>
+  					<option value="international">International</option> 
+  					<option value="opinion" selected>Opinion</option>
+  					<option value="entertainment">Entertainment</option>
+  					<option value="health">Health</option> 
+  					<option value="science" selected>Science</option>
+  					<option value="sports">Sports</option>
+				</select>
+					<input type ="Submit"/>
 			</form>
 
 			)
