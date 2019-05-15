a9cd1f3cfff4f523e1c13143bcb84dc9f5a32f8c
Greg Fruin <gfruin@gmail.com>
2019-05-15 12:14:49 -0500

    working on Login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ArticleContainer from './ArticleContainer/index'
 import Register from './Register'
+import Login from './Login'
 
-function App() {
-  return (
-    <div className="App">
-      <ArticleContainer/>
-      <Register/>
-    </div>
-  );
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			logged: false,
+			username: ''
+		}
+	}
+	login = (username) => {
+		this.setState({
+			logged: true,
+			username: username
+
+		})
+	}
+	render() {
+  		return (
+			<div className="App">
+     	 	<ArticleContainer/>
+     	 	<Register/>
+     	 	<Login login={this.login}/>
+    	 	</div>
+  		);
+	}
 }
+  
+
+
+
 
 export default App;
--- src/Login/index.js
+++ src/Login/index.js
@@ -1,1 +1,49 @@
-index.js
+import React, {Component} from 'react'
+
+
+class Login extends Component {
+	constructor(){
+		super();
+		this.state = {
+			username: '',
+			password: ''
+
+		}
+	}
+	handleChange = (e) => {
+		this.setState({[e.target.name]: e.target.value})
+	}
+	handleSubmit = async (e) => {
+		e.preventDefault()
+		try {
+			const loginResponse = await fetch(process.env.REACT_APP_BACKEND_URL + '/api/v1/auth/login', {
+				method: 'POST',
+				credentials: 'include',
+				body: JSON.stringify(this.state),
+				headers: {
+					'Content-type' : 'application/json'
+				}
+			})
+			const parsedResponse = await loginResponse.json()
+
+			// if(parsedResponse.data === 'login successful') {
+
+			// }
+
+		} catch(err) {
+			console.log(err);
+		}
+	}
+	render() {
+		return(
+			<form onSubmit={this.handleSubmit}>
+			Username:
+			<input type='text' name='username' onChange={this.handleChange}/>
+			Password:
+			<input type='password' name='password' onChange={this.handleChange}/>
+			<button type='submit'> Login </button>
+			</form>
+			)
+	}
+}
+export default Login
