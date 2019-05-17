e591b6296a72fbabd9db77e5e01e6a2b26df947d
Greg Fruin <gfruin@gmail.com>
2019-05-17 10:14:02 -0500

    worked on ArticleList and NewsList css font stylings

--- src/ArticleList/index.js
+++ src/ArticleList/index.js
@@ -5,7 +5,9 @@ const Articles = (props) => {
 	const articleList = props.articles.map((article) => {
 		return (
 			<div key={article._id}>
-				<main className="articleView">{article.title}<br/>
+				<main className="articleView"><br/>
+					<h2>{article.title}</h2>
+					<p className="category">{article.category}</p><br/>
 					<p>{article.description}</p><br/>
 					<button onClick={props.deleteArticle.bind(null, article._id)}>Delete</button>
 					<button onClick={props.showModal.bind(null, article)}>Edit</button>
--- src/NewsList/index.js
+++ src/NewsList/index.js
@@ -4,9 +4,10 @@ const NewsArticles = (props) => {
 	const newsArticleList = props.newsArticles.map((newsArticle, i) => {
 		return (
 			<div key={i}>
-				<main className="articleView">{newsArticle.title}<br/>
-					<p> Source: {newsArticle.source}</p><br/>
-					<p> Description: {newsArticle.description}</p><br/>
+				<main className="articleView"><br/>
+					<h3>{newsArticle.title}</h3>
+					<p className="source"> Source: {newsArticle.source}</p><br/>
+					<p className="description"> Description: {newsArticle.description}</p><br/>
 					<a href={newsArticle.url} target="_blank">View Source Article</a>
 				</main>
 			</div>
